39c0f07430071410468eeb05880a83596e564070
mykhas <m.kobernyk@gmail.com>
2016-04-24 02:41:14 +0300

    Autocomplete module for "From" added

--- src/components/DelaySearch/DelaySearch.js
+++ src/components/DelaySearch/DelaySearch.js
@@ -3,14 +3,6 @@ import AppStore from '../../stores/AppStore.js';
 import AppAction from '../../actions/AppActions.js';
 import Autosuggest from 'react-autosuggest';
 
-const getState = () => {
-  return {
-    airports: AppStore.getAirports(),
-    from: AppStore.getFrom(),
-    to: AppStore.getTo(),
-  };
-}
-
 const getAirports = () => AppStore.getAirports();
 const getFrom = () => AppStore.getFrom();
 const getTo = () => AppStore.getTo();
@@ -19,7 +11,7 @@ const getSuggestions = value => {
   const inputValue = value.trim().toLowerCase();
   const inputLength = inputValue.length;
 
-  return inputLength === 0 ? [] : getState().airports.filter(airport =>
+  return inputLength === 0 ? [] : getAirports().filter(airport =>
     airport.name.toLowerCase().slice(0, inputLength) === inputValue ||
     airport.iata.toLowerCase().slice(0, inputLength) === inputValue
   );
@@ -46,6 +38,8 @@ class DelaySearch extends React.Component {
       suggestionsFrom: getSuggestions(getFrom()),
     };
     this._onChange = this._onChange.bind(this);
+    this.handleChangeTo = this.handleChangeTo.bind(this);
+    this.handleChangeFrom = this.handleChangeFrom.bind(this);
   }
 
   componentWillMount() {
@@ -57,26 +51,45 @@ class DelaySearch extends React.Component {
   }
 
   getState() {
-    this.state = {
+    const state = {
       airports: getAirports(),
       from: getFrom(),
       to: getTo(),
       suggestionsTo: getSuggestions(getTo()),
       suggestionsFrom: getSuggestions(getFrom()),
     };
+    return state;
   }
 
   _onChange() {
     this.setState(this.getState());
   }
 
+  handleChangeTo(event, newValue) {
+    AppAction.selectTo(newValue.newValue);
+  }
+
+  handleChangeFrom(event, newValue) {
+    AppAction.selectFrom(newValue.newValue);
+  }
+
   render() {
+    // const from = this.state.from;
+    const to = this.state.to;
+    const suggestionsFrom = this.state.suggestionsFrom;
+    const suggestionsTo = this.state.suggestionsTo;
+    const inputPropsFrom = {
+      placeholder: 'Type airport name or code',
+      value: this.state.from,
+      onChange: this.handleChangeFrom,
+    };
     return (
-      <Autosuggest suggestions={suggestions}
-         onSuggestionsUpdateRequested={this.onSuggestionsUpdateRequested}
-         getSuggestionValue={getSuggestionValue}
-         renderSuggestion={renderSuggestion}
-         inputProps={inputProps} />
+      <Autosuggest suggestions={suggestionsFrom}
+        onSuggestionsUpdateRequested={this.onSuggestionsUpdateRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputPropsFrom}
+      />
     );
   }
 }
--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,21 +1,33 @@
 import { register } from '../dispatchers/AppDispatcher';
 import AppConstants from '../constants/AppConstants';
 import { EventEmitter } from 'events';
+import fetch from '../core/fetch';
 
 const CHANGE_EVENT = 'change';
 
-let _stack = [];
+let _airports = '';
+let _from = '';
+let _to = '';
+let _delay = '';
 
-let _expression = '';
-let _result = '';
+const _clearFrom = () => { _from = ''; };
+const _clearTo = () => { _to = ''; };
+const _clearDelay = () => { _delay = ''; };
 
-const _addToStack = message => _stack.push({ message });
+const _getAirports = () => _airports;
+const _getDelay = () => _delay;
 
-const _clearStack = () => { _stack = []; };
-
-const _clearExpression = () => { _expression = ''; };
-
-const _clearResult = () => { _result = ''; };
+const _setAirports = () => {
+  const airportsData = fetch('/airports.json').then(function(res) {
+		return res.json();
+	}).then(function(json) {
+		_airports = json;
+	});
+};
+_setAirports();
+const _setFrom = from => { _from = from; };
+const _setTo = to => { _to = to; };
+const _setDelay = delay => { _delay = delay; };
 
 const _interpretExpression = (expression) => {
   try {
@@ -33,8 +45,6 @@ const _interpretExpression = (expression) => {
     _addToStack(`${e.name}: ${e.message}`);
   }
 };
-
-const _setExpression = expression => { _expression = expression; };
 
 const _validateExpression = (expression) => {
   try {
@@ -54,52 +64,56 @@ const _validateExpression = (expression) => {
 }
 
 const AppStore = Object.assign(EventEmitter.prototype, {
-  emitChange(){
-    this.emit( CHANGE_EVENT )
+  emitChange() {
+    this.emit(CHANGE_EVENT);
   },
 
-  addChangeListener(callback){
-    this.on(CHANGE_EVENT, callback)
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
   },
 
-  removeChangeListener(callback){
-    this.removeListener(CHANGE_EVENT, callback)
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
   },
 
-  getStack() {
-    return _stack;
+  getAirports() {
+    return _airports;
   },
 
-  getExpression() {
-    return _expression;
+  getFrom() {
+    return _from;
   },
 
-  getResult() {
-    return _result;
+  getTo() {
+    return _to;
+  },
+
+  getDelay() {
+    return _delay;
   },
 
   dispatcherIndex: register(action => {
     switch (action.actionType) {
       case AppConstants.CLEAR:
-        _clearStack();
-        _clearExpression();
-        _clearResult();
+        _clearFrom();
+        _clearTo();
+        _clearDelay();
         break;
 
-      case AppConstants.INTERPRET:
-        _interpretExpression();
+      case AppConstants.LOAD_AIRPORTS:
+        _getAirports();
         break;
 
-      case AppConstants.PUT_EXPRESSION:
-        _setExpression(action.expression);
+      case AppConstants.LOAD_DELAY:
+        _getDelay();
         break;
 
-      case AppConstants.PUT_STACK:
-        _addToStack(action.message);
+      case AppConstants.SELECT_FROM:
+        _setFrom(action.airport);
         break;
 
-      case AppConstants.VALIDATE:
-        _validateExpression();
+      case AppConstants.SELECT_TO:
+        _setTo(action.airport);
         break;
 
       default:
