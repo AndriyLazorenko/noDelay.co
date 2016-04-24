3e3ab3886e376abf130cc328778a7babaf4275b0
mykhas <m.kobernyk@gmail.com>
2016-04-24 10:46:51 +0300

    Sharing buttons added

--- src/components/DelayData/DelayData.js
+++ src/components/DelayData/DelayData.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import AppStore from '../../stores/AppStore.js';
 import AppAction from '../../actions/AppActions.js';
+import { ShareButtons, ShareCounts, generateShareIcon } from 'react-share';
 import { Row, Column, Link, Callout, Colors } from 'react-foundation';
-import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton
+} = ShareButtons;
+const {
+  FacebookShareCount,
+  GooglePlusShareCount,
+  LinkedinShareCount
+} = ShareCounts;
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+const GooglePlusIcon = generateShareIcon('google');
+const LinkedinIcon = generateShareIcon('linkedin');
 
 const getDelay = () => AppStore.getDelay();
 
@@ -37,20 +52,88 @@ class DelayData extends React.Component {
   }
 
   render() {
+    const shareUrl = 'http://nodelay.co';
+    const title = 'NoDelay — Own your time!';
+
     if (this.state.delay) {
       return (
         <Callout color={Colors.PRIMARY}>
-        <h1 className="{s.title}">Your delay is..</h1>
-        <p>
-        From <strong>{this.state.delay.from}</strong> to
-        <strong> {this.state.delay.to}</strong> minutes
-        with <strong>{this.state.delay.reliability}%</strong>
+          <h1 className="{s.title}">Your delay is..</h1>
+          <p>
+          From <strong>{this.state.delay.from}</strong> to
+          <strong> {this.state.delay.to}</strong> minutes
+          with <strong>{this.state.delay.reliability}%</strong>
 
-        </p>
-        <Link
-          color={Colors.WARNING}
-          onClick={this.clearDelay}
-        >Clear</Link>
+          </p>
+          <Link
+            color={Colors.WARNING}
+            onClick={this.clearDelay}
+          >Clear</Link>
+
+          <div className="Demo__some-network">
+            <FacebookShareButton
+              url={shareUrl}
+              title={title}
+              className="Demo__some-network__share-button">
+              <FacebookIcon
+                size={32}
+                round={true} />
+            </FacebookShareButton>
+
+            <FacebookShareCount
+              url={shareUrl}
+              className="Demo__some-network__share-count">
+              {count => count}
+            </FacebookShareCount>
+          </div>
+
+          <div className="Demo__some-network">
+            <TwitterShareButton
+              url={shareUrl}
+              title={title}
+              className="Demo__some-network__share-button">
+              <TwitterIcon
+                size={32}
+                round={true} />
+            </TwitterShareButton>
+
+            <div className="Demo__some-network__share-count">
+              &nbsp;
+            </div>
+          </div>
+
+          <div className="Demo__some-network">
+            <GooglePlusShareButton
+              url={shareUrl}
+              className="Demo__some-network__share-button">
+              <GooglePlusIcon
+                size={32}
+                round={true} />
+            </GooglePlusShareButton>
+
+            <GooglePlusShareCount
+              url={shareUrl}
+              className="Demo__some-network__share-count">
+              {count => count}
+            </GooglePlusShareCount>
+          </div>
+
+          <div className="Demo__some-network">
+            <LinkedinShareButton
+              url={shareUrl}
+              title={title}
+              className="Demo__some-network__share-button">
+              <LinkedinIcon
+                size={32}
+                round={true} />
+            </LinkedinShareButton>
+
+            <LinkedinShareCount
+              url={shareUrl}
+              className="Demo__some-network__share-count">
+              {count => count}
+            </LinkedinShareCount>
+          </div>
         </Callout>
       );
     } else {
